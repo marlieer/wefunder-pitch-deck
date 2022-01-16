6fabed24f291a432ce8e0efb410c65a06083bce7
Marlie Dueck <marlie_97@hotmail.ca>
2022-01-15 16:24:19 -0800

    scaffold project to use vue and sass

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pitch Deck</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
