5c9848f7b256d739c32ec0358068c8c77efda7a6
Marlie Dueck <marlie_97@hotmail.ca>
2022-01-15 17:32:20 -0800

    make companies list page in vue

--- resources/js/views/Company/CompanyList.vue
+++ resources/js/views/Company/CompanyList.vue
@@ -1,3 +1,34 @@
 <template>
+    <div class="top-right">
+        <button class="secondary-button" @click="createCompany()">Add your company</button>
+    </div>
     <h1>Company Profiles</h1>
+    <div class="companies-list">
+        <CompanyCard v-for="company in companies" :key="company.id" :company="company"/>
+    </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+    data() {
+
+    },
+    computed: {
+        ...mapState([
+            'companies'
+        ]),
+    },
+    methods: {
+        createCompany() {
+            window.location.href = "#add-company"
+        }
+    }
+}
+</script>
+ <style scoped lang="scss">
+ div.top-right {
+     display: flex;
+     justify-content: flex-end;
+ }
+
+</style>
