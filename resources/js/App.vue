5981206e635cc02bf8b77a2efe6db63bda8a7313
Marlie Dueck <marlie_97@hotmail.ca>
2022-01-15 16:24:52 -0800

    vue files skeletons created

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <FileUplaoder></FileUplaoder>
+    </div>
+</template>
+<script>
+import FileUplaoder from './components/Base/FileUploader'
+export default {
+    components: {
+        FileUplaoder,
+    },
+    data() {
+        return {
+            message: 'hello world'
+        }
+    }
+}
+</script>
---
+++ resources/js/components/Base/FileUploader.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <h2>Upload files</h2>
+    <file-pond
+        name="image"
+        ref="pond"
+        label-idle="Click to choose image, or drag here..."
+        label-file-type-not-allowed="Accepted file types are powerpoint or pdf"
+        :accepted-file-types="acceptedFileTypes"
+        server="/api/pitch-deck"/>
+</div>
+</template>
+<script>
+import vueFilePond from 'vue-filepond'
+import "filepond/dist/filepond.min.css"
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+export default {
+    components: {
+        FilePond    // file-pond
+    },
+    data() {
+        return {
+            acceptedFileTypes: [    // accepts powerpoint and pdf 
+                "application/pdf", 
+                "application/vnd.ms-powerpoint", 
+                "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+            ] 
+        }
+    }
+}
+</script>
